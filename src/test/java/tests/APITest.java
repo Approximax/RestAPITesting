ed94ee13ac3aba2bccf7a3063aef1748034cbfe9
Vladislav Kryukov <vlad_pw_v@list.ru>
2024-03-17 13:21:25 +0300

    API tests

--- src/test/java/tests/APITest.java
+++ src/test/java/tests/APITest.java
@@ -7,54 +7,81 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 public class APITest {
 
-    private static String base = "https://reqres.in/";
+    private final String base = "https://reqres.in/";
 
     @Test
     @Tag("reqres")
-    void singleUserTest () {
-        given()
-                .when()
-                    .get(base + "api/users/2")
-                .then()
-                    .log().status()
-                    .log().body()
-                    .statusCode(200);
-    }
+    @DisplayName("Успешная регистрация с корректными данными")
+    void successfulLogin() {
 
-    @Test
-    @Tag("reqres")
-    @DisplayName("Проверка кода ответа, при POST создания пользователя")
-    void checkStatusCodeCreate() {
-        given()
-                .log().uri()
-                .when()
-                    .post(base + "/api/users")
-                .then()
-                    .log().status()
-                    .log().body()
-                    .statusCode(201);
-    }
-
-    @Test
-    void validateResponse() {
-        String test_data = "{\"name\": \"morpheus\", \"job\": \"zion resident\"}";
+        String test_data = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\" }";
 
         given()
                 .log().uri()
                 .contentType(JSON)
                 .body(test_data)
-        .when()
-                .put(base + "api/users/2")
-        .then()
+                .when()
+                .post(base + "api/login")
+                .then()
                 .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    @Tag("reqres")
+    @DisplayName("Успешная регистрация с минимальными данными")
+    void successfulRegistration() {
+        String test_credentials =  "{ \"email\": \"java_chemp@apr.in\", \"password\": \"overcome\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(test_credentials)
+                .when()
+                .post(base + "api/registration")
+                .then()
                 .log().status()
+                .log().body()
                 .statusCode(201)
-                .body("name", is(""))
+                .body("email", is("java_chemp@apr.in"),
+                        "password", is("overcome"));
+    }
 
+    @Test
+    @Tag("reqres")
+    @DisplayName("Некорректная регистрация с отсутствующим паролем")
+    void unsuccessfulRegistration() {
+        String test_login = "{\"email\": \"sydney@fife\"}";
 
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(test_login)
+                .when()
+                .post(base + "api/register")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+    @Test
+    @Tag("reqres")
+    @DisplayName("Пользователь не найден")
+    void userNotFound() {
+        given()
+                .log().uri()
+                .when()
+                .get(base + "api/users/23")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(404);
     }
 }
